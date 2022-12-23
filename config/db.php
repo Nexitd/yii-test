8282511bf92c59929a0ef537b5d224a509ce8f27
Nexitd <kern45061@gmail.com>
2022-12-23 11:48:00 +0300

    test

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'pgsql:host=localhost;port=5432;dbname=test',
-    'username' => 'nexitd',
-    'password' => 'asD3r45ty67',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
 ];
